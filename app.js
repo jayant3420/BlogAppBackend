3cb1378052cd30202a22e525bd3960cd056a1362
jayant kumar <jayant.3420@gmail.com>
2021-08-08 13:16:30 +0530

    ddd

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const routerHome = require("./routes/home");
 const routerBollywood = require("./routes/bollywood");
 const routerReactBlog = require("./routes/reactblog");
 const PORT = process.env.PORT || 3001;
+const cors = require("cors");
 
 //HTTP logger
-app.use((req, res, next) => {
-  res.setHeader("Allow-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Allow-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Headers", "*");
+//   next();
+// });
+app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use("/home", routerHome);
